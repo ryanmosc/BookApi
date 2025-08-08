654aa43195de698de04ffd402072d9b8931c35d0
ryanmosc <ryanoliveiramosc.com.098@gmail.com>
2025-08-08 14:19:55 -0300

    Adicionei validaçõa de body

--- src/main/java/com/bookApi/demo/infra/RestExceptionHandler.java
+++ src/main/java/com/bookApi/demo/infra/RestExceptionHandler.java
@@ -3,21 +3,54 @@ package com.bookApi.demo.infra;
 import com.bookApi.demo.Exceptions.BookNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(BookNotFoundException.class)
-    private ResponseEntity<RestErrorMessage> bookNotFoundHandler(BookNotFoundException exception){
+    private ResponseEntity<RestErrorMessage> bookNotFoundHandler(BookNotFoundException exception) {
         RestErrorMessage threatResponse = new RestErrorMessage(
                 exception.getMessage(),
                 HttpStatus.NOT_FOUND,
                 HttpStatus.NOT_FOUND.value(),
-                LocalDateTime.now());
-        return  ResponseEntity.status(HttpStatus.NOT_FOUND).body(threatResponse);
+                LocalDateTime.now()
+        );
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(threatResponse);
+    }
+
+
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  WebRequest request) {
+        String errorMessage = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(fieldError -> fieldError.getField() + ": " + fieldError.getDefaultMessage())
+                .collect(Collectors.joining(", "));
+
+        RestErrorMessage threatResponse = new RestErrorMessage(
+                "Erro de validação: " + errorMessage,
+                HttpStatus.BAD_REQUEST,
+                HttpStatus.BAD_REQUEST.value(),
+                LocalDateTime.now()
+        );
+        return new ResponseEntity<>(threatResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    private ResponseEntity<RestErrorMessage> internalServerErrorHandler(Exception exception) {
+        RestErrorMessage threatResponse = new RestErrorMessage(
+                "Ocorreu um erro interno no servidor: " + exception.getMessage(),
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                LocalDateTime.now()
+        );
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(threatResponse);
     }
 }
